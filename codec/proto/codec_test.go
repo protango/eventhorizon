8ccb97fb0b6b575e20bd98c5cea446528bee895e
Zac Murray <zac@paypaplane.com>
2021-08-27 04:29:21 +0000

    Add tests for proto codec

--- codec/proto/codec_test.go
+++ codec/proto/codec_test.go
@@ -15,36 +15,79 @@
 package proto
 
 import (
-	"strings"
+	"bytes"
+	"context"
 	"testing"
+	"time"
 
-	"github.com/looplab/eventhorizon/codec"
+	eh "github.com/looplab/eventhorizon"
+	"github.com/looplab/eventhorizon/mocks"
+	"github.com/looplab/eventhorizon/uuid"
+	"google.golang.org/protobuf/proto"
+	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func TestEventCodec(t *testing.T) {
+	// Setup test parameters
+	eventType := eh.EventType("CodecEvent")
+	ctx := mocks.WithContextOne(context.Background(), "testval")
+	id := uuid.MustParse("10a7ec0f-7f2b-46f5-bca1-877b6e33c9fd")
+	timestamp := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
+	metadata := map[string]interface{}{"num": 42.0}
+	eventData := TestData{
+		Value: "Test data",
+	}
+
+	// Register with eh
+	eh.RegisterEventData(eventType, func() eh.EventData { return &TestData{} })
+
+	// Init codec
 	c := &EventCodec{}
-	expectedBytes := strings.ReplaceAll(strings.ReplaceAll(strings.ReplaceAll(`
-	{
-		"event_type": "CodecEvent",
-		"data": {
-		  "Bool": true,
-		  "String": "string",
-		  "Number": 42,
-		  "Slice": ["a", "b"],
-		  "Map": { "key": "value" },
-		  "Time": "2009-11-10T23:00:00Z",
-		  "TimeRef": "2009-11-10T23:00:00Z",
-		  "NullTime": null,
-		  "Struct": { "Bool": true, "String": "string", "Number": 42 },
-		  "StructRef": { "Bool": true, "String": "string", "Number": 42 },
-		  "NullStruct": null
-		},
-		"timestamp": "2009-11-10T23:00:00Z",
-		"aggregate_type": "Aggregate",
-		"aggregate_id": "10a7ec0f-7f2b-46f5-bca1-877b6e33c9fd",
-		"version": 1,
-		"metadata": { "num": 42 },
-		"context": { "context_one": "testval" }
-	}`, " ", ""), "\n", ""), "\t", "")
-	codec.EventCodecAcceptanceTest(t, c, []byte(expectedBytes))
+
+	// find expected bytes
+	eventDataBytes, err := proto.Marshal(&eventData)
+	if err != nil {
+		t.Error("Failed to set up expected bytes:", err)
+	}
+
+	ehEvent := Event{
+		EventType:     string(eventType),
+		Timestamp:     timestamppb.New(timestamp),
+		AggregateType: string(mocks.AggregateType),
+		AggregateID:   id.String(),
+		Version:       1,
+		Context:       []byte(`{"context_one":"testval"}`),
+		Metadata:      []byte(`{"num":42}`),
+		RawData:       eventDataBytes,
+	}
+
+	expectedBytes, err := proto.Marshal(&ehEvent)
+	if err != nil {
+		t.Error("Failed to set up expected bytes:", err)
+	}
+
+	// Marshaling.
+	event := eh.NewEvent(eventType, &eventData, timestamp,
+		eh.ForAggregate(mocks.AggregateType, id, 1),
+		eh.WithMetadata(metadata), // NOTE: Just one key to avoid comparisson issues.
+	)
+	b, err := c.MarshalEvent(ctx, event)
+	if err != nil {
+		t.Error("there should be no error:", err)
+	}
+	if !bytes.Equal(b, expectedBytes) {
+		t.Error("the encoded bytes should be correct:", b)
+	}
+
+	// Unmarshaling.
+	decodedEvent, decodedContext, err := c.UnmarshalEvent(context.Background(), b)
+	if err != nil {
+		t.Error("there should be no error:", err)
+	}
+	if err := eh.CompareEvents(decodedEvent, event, eh.CompareDataAsProto()); err != nil {
+		t.Error("the decoded event was incorrect:", err)
+	}
+	if val, ok := mocks.ContextOne(decodedContext); !ok || val != "testval" {
+		t.Error("the decoded context was incorrect:", decodedContext)
+	}
 }
